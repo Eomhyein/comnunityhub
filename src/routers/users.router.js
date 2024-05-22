d2196b974788062bd2324b67bd78ae30db7123ce
hyein <mickey_8688@naver.com>
2024-05-22 20:15:46 +0900

    user login API

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js'; // 데이터베이스에 있는 Users조회
 import bcrypt from 'bcrypt'; // 1-5. bcrypt 리팩토링
+import jwt from 'jsonwebtoken'; // 2. 로그인 인증 미들웨어
 
 const router = express.Router();
 
@@ -27,7 +28,7 @@ router.post('/sign-up', async (req, res, next) => {
   const user = await prisma.users.create({
     data: { 
       email, 
-      password: hashedPassword, // 암호화된 비밀번호를 저장합니다.
+      password: hashedPassword, // 1-5. 암호화된 비밀번호를 저장합니다.
     },
   });
 
@@ -43,6 +44,32 @@ router.post('/sign-up', async (req, res, next) => {
   });
 
   return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+});
+
+/*2. 사용자 로그인 API*/
+router.post('/sign-in', async (req, res, next) => {
+  // 2-1 "email", "password"를 **body**로 전달받습니다.
+  const { email, password } = req.body;
+  // 2-2 전달 받은 'email'에 해당하는 사용자가 있는지 확인합니다.
+  const user = await prisma.users.findFirst({ where: { email } });
+  
+  if (!user)
+    return res.status(401).json({ message: '존재하지 않는 이메일입니다.' });
+  
+  // 2-3 전달 받은 'password'와 데이터베이스의 저장된 'password'를 'bcrypt를 이용해 검증합니다.
+  else if (!(await bcrypt.compare(password, user.password)))
+    return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+
+  // 2-4  로그인에 성공하면, 사용자에게 JWT 토큰을 발급합니다.
+  const token = jwt.sign(
+    {
+      userId: user.userId,
+    },
+    'custom-secret-key', //비밀키, dodtenv를 이용해서, 외부에서 코드 못보게!
+  );
+
+  res.cookie('authorization', `Bearer ${token}`);
+  return res.status(200).json({ message: '로그인 성공' });
 });
 
 export default router;
