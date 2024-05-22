4aebc58a24a66c102f4e24596dcd4a6ff3f05f29
hyein <mickey_8688@naver.com>
2024-05-22 18:36:45 +0900

    sign-up 02  Bcrpyt

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,6 +1,7 @@
 // src/routes/users.router.js
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js'; // 데이터베이스에 있는 Users조회
+import bcrypt from 'bcrypt'; // 1-5. bcrypt 리팩토링
 
 const router = express.Router();
 
@@ -19,9 +20,15 @@ router.post('/sign-up', async (req, res, next) => {
     return res.status(409).json({ message: '이미 존재하는 이메일입니다.' });
   }
 
+  // 1-5. 사용자 비밀번호를 암호화합니다.
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   // 1-3. **Users** 테이블에 `email`, `password`를 이용해 사용자를 생성합니다.
   const user = await prisma.users.create({
-    data: { email, password },
+    data: { 
+      email, 
+      password: hashedPassword, // 암호화된 비밀번호를 저장합니다.
+    },
   });
 
   // 1-4. **UserInfos** 테이블에 `name`, `age`, `gender`, `profileImage`를 이용해 사용자 정보를 생성합니다.
